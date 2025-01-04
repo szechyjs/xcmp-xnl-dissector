51620b149537689d97b4b49585bcda2121f8cf42
Jared Szechy <jared.szechy@gmail.com>
2025-01-04 01:17:26 -0500

    Decode nested super bundle msgs & add more fields

--- 2-xcmp.lua
+++ 2-xcmp.lua
@@ -19,12 +19,15 @@ local opcodes_base = {
   [0x0012] = "UUID",
   [0x0016] = "RXBERCTRL",
   [0x0017] = "RXBERSYNC",
+  [0x0019] = "DATECODE",
+  [0x001f] = "TANAPA",
   [0x0023] = "DSCREMDEV",
   [0x0024] = "REMCONNECT",
   [0x0025] = "REMDISCONNECT",
   [0x002b] = "LANGPK",
   [0x002c] = "LANGPKINFO",
   [0x002e] = "SUPERBUNDLE",
+  [0x0030] = "CONNTEST",
   [0x0037] = "CPATTR",
   [0x003d] = "SECCONN",
   [0x0100] = "READISHITEM",
@@ -38,10 +41,14 @@ local opcodes_base = {
   [0x0108] = "ISHUNLOCKPRT",
   [0x0109] = "CLONEWR",
   [0x010a] = "CLONERD",
+  [0x010b] = "PSDTACCESS",
+  [0x010c] = "RUPDATE",
   [0x010e] = "COMPREAD",
   [0x010f] = "COMPSESS",
   [0x0200] = "BOOTMODE",
-  [0x0201] = "MEM",
+  [0x0201] = "READMEM",
+  [0x0202] = "WRITEMEM",
+  [0x0203] = "ERASEFLASH",
   [0x0204] = "BOOTJMPEXEC",
   [0x0206] = "BOOTWRITECMT",
   [0x0207] = "REMDUP",
@@ -78,16 +85,20 @@ local opcodes_base = {
   [0x042e] = "ALARMCTRL",
   [0x042f] = "ROSCTRL",
   [0x0432] = "DATETIME",
+  [0x0434] = "NETCFG",
   [0x0440] = "MEMSTRMREAD",
   [0x0441] = "MEMSTRMWRITE",
   [0x0443] = "NANDACCESS",
   [0x0444] = "FTLACCESS",
   [0x0445] = "FILEACCESS",
+  [0x0446] = "XFERDATA",
   [0x0447] = "RPTRCTRL",
   [0x0458] = "FD",
   [0x0461] = "MODINFO",
   [0x0467] = "CPPASSWDLCK",
   [0x046c] = "UNKILL",
+  [0x0480] = "CERTMGT",
+  [0x048a] = "SECCERTMGT",
   [0x04a1] = "SWA_AUDIO",
 }
 
@@ -123,12 +134,31 @@ local calltypes = {
 
 local results = {
   [0] = "Success",
+  [1] = "Failure",
   [2] = "Incorrect Mode",
-  [3] = "Unsupported Opcode",
+  [3] = "Opcode Not Supported",
   [4] = "Invalid Parameter",
   [5] = "Reply Too Big",
   [6] = "Security Locked",
-  [7] = "Unavailable Function",
+  [7] = "Bundled Opcode Not Supported",
+  [16] = "Busy",
+  [17] = "Bit Locked",
+  [18] = "Radio Is Locked",
+  [19] = "Voltage Not Stable",
+  [20] = "Program Failure",
+  [22] = "Transfer Complete",
+  [23] = "Request Not RXed",
+  [64] = "Softpot Operation Not Supported",
+  [65] = "Softpot Type Not Supported",
+  [66] = "Softpot Value Out of Range",
+  [128] = "Flash Write Failure",
+  [129] = "ISH Item Not Found",
+  [130] = "ISH Offset Out of Range",
+  [131] = "ISH Insufficient Partition Space",
+  [132] = "ISH Partition Does Not Exist",
+  [133] = "ISH Partition Read Only",
+  [134] = "ISH Reorg Needed",
+  [135] = "Undefined",
 }
 
 local devinitsts_inits = {
@@ -277,6 +307,38 @@ local func_ops = {
   [2] = "Read IP and port",
 };
 
+local ish_types = {
+  [10] = "RAD_CFG_CMP_TYPE",
+  [12] = "GENERAL",
+  [38] = "CNV_RAD_CMP_TYPE",
+  [52] = "SCAN_RAD_CMP_TYPE",
+  [67] = "VOICE ANNOUNCEMENT_CMP_TYPE",
+  [71] = "RAD_APP_INFO_CMP_TYPE",
+  [98] = "QCK_TXT_MSG_LIST_TYPE",
+  [114] = "NETSET_CMP_TYPE",
+  [151] = "WIFINETWORK_DLH_TYPE",
+  [3925] = "WIFINETWORK_DLL_TYPE",
+  [3970] = "ACTIONLIST_CMP_TYPE",
+  [3988] = "SLT_5_DECODER_CMP_TYPE",
+  [4002] = "CAPACITY_PLUS_UCL_DLL_TYPE",
+  [4007] = "TRUNK_PER_LIST_DLH_TYPE",
+  [4010] = "ST_SRCH_DLH_TYPE",
+  [4011] = "EHNPRI_CMP_TYPE",
+  [4020] = "RG_TG_LIST_DLL_TYPE",
+  [4021] = "RX_TG_LIST_DLH_TYPE",
+  [4030] = "DIGITAL_UCL_DLL_TYPE",
+  [4044] = "MDC_SYS_CMP_TYPE",
+  [4081] = "SCAN_PER_LIST_TYPE",
+  [4091] = "CNV_PER_CMP_TYPE",
+  [4092] = "ZN_PER_ASGN_LIST_TYPE",
+  [4093] = "ZN_PER_ASGN_DLL_TYPE",
+  [4096] = "TEL_CMP_TYPE",
+  [4112] = "RADIO_SECURITY_INFO_CMP_TYPE",
+  [4113] = "FEAT_DESCR_CMP_TYPE",
+  [4116] = "LTD_CFS_CMP_TYPE",
+  [4400] = "LTD_CFS_CMP_TYPE",
+};
+
 local f_opcode = ProtoField.uint16("xcmp.opcode", "Opcode", base.HEX, opcodes)
 local f_result = ProtoField.uint8("xcmp.result", "Result", base.DEC, results)
 local f_address_type = ProtoField.uint8("xcmp.address.type", "Type", base.DEC, address_types)
@@ -317,7 +379,7 @@ local f_cpattr = ProtoField.uint8("xcmp.cpattr.attr", "Codeplug Attribute", base
 local f_cpattr_len = ProtoField.uint8("xcmp.cpattr.op", "Codeplug Attribute Length", base.DEC)
 local f_cpattr_value = ProtoField.bytes("xcmp.cpattr.value", "Codeplug Attribute Value")
 local f_readish_lp = ProtoField.uint8("xcmp.readish.lp", "ISH Logical Partition", base.DEC)
-local f_readish_type = ProtoField.uint16("xcmp.readish.type", "ISH Type", base.DEC)
+local f_readish_type = ProtoField.uint16("xcmp.readish.type", "ISH Type", base.DEC, ish_types)
 local f_readish_id = ProtoField.uint16("xcmp.readish.id", "ISH ID", base.DEC)
 local f_readish_len = ProtoField.uint16("xcmp.readish.len", "ISH Number of Bytes", base.DEC)
 local f_readish_offset = ProtoField.uint16("xcmp.readish.offset", "ISH Offset", base.DEC)
@@ -345,6 +407,21 @@ local f_sb_cont_fail = ProtoField.uint8("xcmp.sb.cont_fail", "Continue on Fail",
 local f_sb_msg = ProtoField.bytes("xcmp.sb.msg", "Message")
 local f_sb_msg_len = ProtoField.uint16("xcmp.sb.msg_len", "Length", base.DEC)
 local f_sb_msg_data = ProtoField.bytes("xcmp.sb.msg_data", "Data")
+local f_tanapa = ProtoField.string("xcmp.tanapa", "TANAPA Number")
+local f_rs_serial = ProtoField.string("xcmp.rs.serial", "RS_SERIALNUM")
+local f_rs_model = ProtoField.string("xcmp.rs.model", "RS_MODELNUM")
+local f_rs_majsecver = ProtoField.uint8("xcmp.rs.majsecver", "RS_MAJSECVERS", base.HEX)
+local f_rs_midsecver = ProtoField.uint8("xcmp.rs.midsecver", "RS_MIDSECVERS", base.HEX)
+local f_rs_minsecver = ProtoField.uint8("xcmp.rs.minsecver", "RS_MINSECVERS", base.HEX)
+local f_rs_orgprogday = ProtoField.uint8("xcmp.rs.orgprogday", "RS_ORGPROGDAY", base.HEX)
+local f_rs_orgprogmonth = ProtoField.uint8("xcmp.rs.orgprogmonth", "RS_ORGPROGMONTH", base.HEX)
+local f_rs_orgprogyear = ProtoField.uint8("xcmp.rs.orgprogyear", "RS_ORGPROGYEAR", base.HEX)
+local f_rs_orgproghour = ProtoField.uint8("xcmp.rs.orgproghou", "RS_ORGPROGHOUR", base.HEX)
+local f_rs_orgprogmins = ProtoField.uint8("xcmp.rs.orgprogmins", "RS_ORGPROGMINUTES", base.HEX)
+local f_rs_rgnid = ProtoField.uint8("xcmp.rs.rgnid", "RS_RGNID", base.DEC)
+local f_rs_procid = ProtoField.bytes("xcmp.rs.procid", "RS_PROCID")
+local f_netset_ip = ProtoField.ipv4("xcmp.netset.ip", "NETSET_RDIPADDR")
+local f_netset_msk = ProtoField.ipv4("xcmp.netset.msk", "NETSET_RDSUBMASK")
 
 proto.fields = {
   f_opcode,
@@ -415,6 +492,21 @@ proto.fields = {
   f_sb_msg,
   f_sb_msg_len,
   f_sb_msg_data,
+  f_tanapa,
+  f_rs_serial,
+  f_rs_model,
+  f_rs_majsecver,
+  f_rs_midsecver,
+  f_rs_minsecver,
+  f_rs_orgprogday,
+  f_rs_orgprogmonth,
+  f_rs_orgprogyear,
+  f_rs_orgproghour,
+  f_rs_orgprogmins,
+  f_rs_rgnid,
+  f_rs_procid,
+  f_netset_ip,
+  f_netsel_msk,
 }
 
 -- dofile("xnl.luainc") -- uncomment to fix dependency order
@@ -442,16 +534,24 @@ function proto.dissector(buf, pkt, root)
   end
 
   local tree = root:add(proto, buf(0, buf:len()))
+
+  desc = process_xcmp(buf, tree)
+
+  pkt.cols.protocol:set("XCMP")
+  pkt.cols.info:set(desc)
+end
+
+function process_xcmp(buf, tree)
   local opcode = buf(0, 2):uint()
   tree:add(f_opcode, buf(0, 2))
-
-  local desc = (opcodes[opcode] or opcode) .. " Transaction=" .. xnl_transaction().value
 
   local result = nil
   if (opcode >> 12) == 8 then
     result = buf(2, 1):uint()
     tree:add(f_result, buf(2, 1))
   end
+
+  local desc = (opcodes[opcode] or opcode) .. " Transaction=" .. xnl_transaction().value
 
   if opcode == 0x000e then
     tree:add(f_rstatus_cond, buf(2, 1))
@@ -484,6 +584,8 @@ function proto.dissector(buf, pkt, root)
     tree:add(f_serial_value, buf(3, buf:len() - 3))
   elseif opcode == 0x8012 then
     tree:add(f_uuid, buf(3, 16))
+  elseif opcode == 0x801f then
+    tree:add(f_tanapa, buf(3))
   elseif opcode == 0x002c then
     tree:add(f_langpk_op, buf(2, 1))
     desc = desc .. " Op=" .. buf(2, 1):uint()
@@ -520,9 +622,10 @@ function proto.dissector(buf, pkt, root)
     local index = 4
     for i = 0, buf(2, 1):uint() - 1 do
       local len = buf(index, 2):uint()
-      local msg = tree:add(f_sb_msg, buf(index, len))
+      local msg = tree:add(f_sb_msg, buf(index, len + 2))
       msg:add(f_sb_msg_len, buf(index, 2))
       msg:add(f_sb_msg_data, buf(index + 2, len))
+      process_xcmp(buf(index + 2, len), msg)
       index = index + 2 + len
     end
   elseif opcode == 0x802e then
@@ -530,9 +633,10 @@ function proto.dissector(buf, pkt, root)
     local index = 4
     for i = 0, buf(3, 1):uint() - 1 do
       local len = buf(index, 2):uint()
-      local msg = tree:add(f_sb_msg, buf(index, len))
+      local msg = tree:add(f_sb_msg, buf(index, len + 2))
       msg:add(f_sb_msg_len, buf(index, 2))
       msg:add(f_sb_msg_data, buf(index + 2, len))
+      process_xcmp(buf(index + 2, len), msg)
       index = index + 2 + len
     end
   elseif opcode == 0x0037 then
@@ -566,11 +670,31 @@ function proto.dissector(buf, pkt, root)
     tree:add(f_readish_lp, lp)
     tree:add(f_readish_type, type)
     tree:add(f_readish_id, id)
-    local len = buf(8, 2):uint()
-    tree:add(f_readish_len, len)
+    local num_bytes = buf(8, 2)
+    tree:add(f_readish_len, num_bytes)
     tree:add(f_readish_offset, buf(10, 2))
     tree:add(f_readish_itmsz, buf(12, 2))
-    tree:add(f_readish_data, buf(14, len))
+    if num_bytes:uint() > 0 then
+      tree:add(f_readish_data, buf(14, num_bytes:uint()))
+
+      if type:uint() == 114 then
+        tree:add(f_netset_msk, buf(14 + 4, 4))
+        tree:add(f_netset_ip, buf(14 + 8, 4))
+      elseif type:uint() == 4112 then
+        tree:add(f_rs_serial, buf(14 + 0, 10))
+        tree:add(f_rs_model, buf(14 + 12, 12))
+        tree:add(f_rs_majsecver, buf(14 + 24, 1))
+        tree:add(f_rs_midsecver, buf(14 + 25, 1))
+        tree:add(f_rs_minsecver, buf(14 + 26, 1))
+        tree:add(f_rs_orgprogyear, buf(14 + 30, 1))
+        tree:add(f_rs_orgprogmonth, buf(14 + 31, 1))
+        tree:add(f_rs_orgprogday, buf(14 + 32, 1))
+        tree:add(f_rs_orgproghour, buf(14 + 33, 1))
+        tree:add(f_rs_orgprogmins, buf(14 + 34, 1))
+        tree:add(f_rs_rgnid, buf(14 + 78, 1))
+        tree:add(f_rs_procid, buf(14 + 96, 8))
+      end
+    end
     desc = desc .. " LP=" .. lp:uint() .. " Type=" .. type:uint() .. " ID=" .. id:uint()
   elseif opcode == 0x0104 then
     local lp = buf(2, 1)
@@ -655,6 +779,5 @@ function proto.dissector(buf, pkt, root)
     desc = desc .. " Fn=" .. buf(2, 1):uint()
   end
 
-  pkt.cols.protocol:set("XCMP")
-  pkt.cols.info:set(desc)
+  return desc
 end
